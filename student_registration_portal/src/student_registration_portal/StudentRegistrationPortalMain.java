489ed0b4b62c6c41594633a457d240b8a221d921
Student <Student@LabVM2049939.ew0a3xeclj0uzlf1guv14y0keh.jnbx.internal.cloudapp.net>
2026-09-18 19:10:19 +0200

    Ask student to capture firstname, lastname and username with validation to the username.

--- student_registration_portal/src/student_registration_portal/StudentRegistrationPortalMain.java
+++ student_registration_portal/src/student_registration_portal/StudentRegistrationPortalMain.java
@@ -15,19 +15,48 @@ import java.util.Scanner;
 public class StudentRegistrationPortalMain {
 
     public static Scanner input = new Scanner(System.in);
-    
-    public String firstname;
-    public String lastname;
-    public String username;
-    public String password;
-    public String cellPhoneNumber;
-    
-    
+        
+   
     public static void main(String[] args) {
+        
+        String firstname;
+        String lastname;
+        String username;
+        String password;
+        String cellPhoneNumber;
     
-        System.out.println("===== Welcome to the Student Registration Portal =======");
+        System.out.println("===== Welcome to the Student Registration Portal =======\n");
+        
+        // Ask and capture for student details 
+        System.out.print("Enter your firstname: ");
+        firstname = input.nextLine();
+        
+        System.out.print("Enter your surname: ");
+        lastname = input.nextLine();
+        
+        System.out.println("Thank you : " +firstname+" "+lastname+".\n");
+        
+       // Ask student username and validate it.
+       while (true) {
+           System.out.print("Please enter your preferred username: ");
+           username = input.nextLine();
+           
+           if (checkStudentUsername(username)) {
+               System.out.println("Username successfully captured!");
+               break;
+           } else {
+               System.out.println("Invaild username. It must contain an unerscore (_) and be on more than 5 characters");
+               System.out.println("");
+           }
+       }
+       
+       
         
         input.close();
+    }
+    
+    public static boolean checkStudentUsername(String prefferredUsername) {
+      return prefferredUsername.contains("_") && prefferredUsername.length() >= 4;
     }
     
 }
